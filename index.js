44e9e967815e453acb33b98e9063a0274fd3d539
Heghine <heghine.azatyan99@gmail.com>
2024-11-10 22:11:24 +0400

    fix changes

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-import 'react-native-gesture-handler';
+import { registerRootComponent } from 'expo';
 
-import { AppRegistry } from 'react-native';
+import App from './App';
 
-import { name as appName } from './app.json';
-import RootApp from './src';
-
-if (__DEV__) {
-  import('./src/ReactotronConfig').then(() => {});
-}
-
-AppRegistry.registerComponent(appName, () => RootApp);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
